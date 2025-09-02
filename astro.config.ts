30f7bea58e72b62ab2001d43cfd70192b02bd97d
Zacharias Enochsson <zacharias.enochsson@gmail.com>
2025-09-02 16:51:51 +0200

    seo improvements hopefully

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config"
 import hyperapp from "astrojs-hyperapp"
 import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://zaceno.github.io",
   integrations: [
+    sitemap(),
     hyperapp(),
     mdx({
       shikiConfig: {
